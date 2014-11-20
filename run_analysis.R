db76b5585b73a111e7897c28b8b268d7b1e889d2
Jeremy Finke <jeremy.finke@kc.frb.org>
2014-11-20 15:05:34 -0600

    Loading the dataframes

--- run_analysis.R
+++ run_analysis.R
@@ -5,4 +5,21 @@
 # Appropriately labels the data set with descriptive variable names. 
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+# Load libraries for processing
+library("plyr")
 
+# Load X_test data
+X_test <- read.table("X_test.txt", quote="\"")
+
+# Load X_train data
+X_train <- read.table("X_train.txt", quote="\"")
+
+# Load features
+features <- read.table("features.txt", quote="\"")
+# Transform features
+headings<-features[2]
+theadings<-t(headings)
+theadings2<-as.data.frame(theadings)
+
+
+X <- rbind.fill(theadings2,X_test, X_train) #combining the three dataframes
